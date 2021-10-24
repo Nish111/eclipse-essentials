b397032cb389652620ce24767a00a0c45b762f42
Nishant Banginwar <nishanba@amdocs.com>
2021-10-24 12:30:59 +0530

    Removed Comment

--- src/simpleone/Examples.java
+++ src/simpleone/Examples.java
@@ -3,7 +3,6 @@ package simpleone;
 public class Examples {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Doing commit in Git local repo");
 
 	}
